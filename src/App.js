f1b792b5cb8b03139cc943e6c3a216e3bedadec8
skobelev denis <generaldennis88@gmail.com>
2022-06-27 11:53:21 +0200

    try fix basename v4

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ import Recipe from "./pages/Recipe";
 function App() {
   return (
     <>
-      <Router basename="/react-spa-shop/">
+      <Router basename="/react-spa-shop">
         <Header/>
           <main className="container content">
               <Routes>
-                <Route path="/" element={ <Home/> }/>
-                <Route path="/about" element={ <About/> }/>
-                <Route path="/contacts" element={ <Contacts/> }/>
-                <Route path="/category/:name" element={ <Category/> }/>
-                <Route path="/recipe/:id" element={ <Recipe/> }/>
+                <Route path="/react-spa-shop" element={ <Home/> }/>
+                <Route path="/react-spa-shop/about" element={ <About/> }/>
+                <Route path="/react-spa-shop/contacts" element={ <Contacts/> }/>
+                <Route path="/react-spa-shop/category/:name" element={ <Category/> }/>
+                <Route path="/react-spa-shop/recipe/:id" element={ <Recipe/> }/>
                 <Route path="*" element={ <NotFound/> }/>
               </Routes>
           </main>
